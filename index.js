25f007ff1c1fa5b059b736c1f94099ca831aaa94
aasim-syed <70385414+aasim-syed@users.noreply.github.com>
2021-09-12 16:43:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+     var dark=document.getElementById('but').addEventListener('mousedown',darkmode)
+function darkmode () {
+  
+  document.body.style.background = '#0B0B0B';
+  document.getElementById('projects').style.color="#1AEB5F";
+  // body... 
+  document.getElementById('contact').style.color="#1AE929";
+   document.getElementById('contact').style.background ="#1C1010";
+  document.getElementsByClassName('about__content-skills').style.background="#100F0F";
+  document.getElementsByClassName('header').className="bg-dark";
+}
