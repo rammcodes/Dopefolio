edb96b828bc9af7638b206a8cb8800842a3bcde3
Ram Maheshwari <ram991011@gmail.com>
2021-09-14 18:16:03 +0530

    added close icon to close small menu

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // ---
 const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
 const smallMenu = document.querySelector('.header__sm-menu')
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu');
+const headerHamMenuCloseBtn = document.querySelector('.header__main-ham-menu-close');
 
 hamMenuBtn.addEventListener('click', () => {
   if (smallMenu.classList.contains('header__sm-menu--active')) {
     smallMenu.classList.remove('header__sm-menu--active')
   } else {
     smallMenu.classList.add('header__sm-menu--active')
+  }
+  if(headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none');
+    headerHamMenuCloseBtn.classList.add('d-none');
+  }else {
+    headerHamMenuBtn.classList.add('d-none');
+    headerHamMenuCloseBtn.classList.remove('d-none');
   }
 })
 
