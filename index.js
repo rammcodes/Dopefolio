8573a7d1ffd8ffed70c715e040afb4ec8b8f7bb1
CodeMode365 <codemode365@gmail.com>
2023-04-21 16:40:32 +0545

    Active navigation view for Larger screen

--- index.js
+++ index.js
@@ -36,3 +36,39 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+
+// show up active navigation
+const nav = Array.from(document.getElementById("navigation").children)
+const sections =[eleById("home"),eleById("about"),eleById("projects"),eleById("contact")]
+
+window.onscroll =()=>{
+  removeActive()
+  addActive()
+}
+//return element 
+function eleById(id){
+  return document.getElementById(id)
+}
+
+function removeActive(){
+  nav.forEach(navEle=>{
+    navEle.children[0].classList.remove("active-nav")
+  })
+}
+
+function addActive(){
+    if(window.scrollY >= 0 && window.scrollY <= sections[1].offsetTop-50){
+      nav[0].children[0].classList.add("active-nav")
+    }
+    else if(window.scrollY >= sections[1].offsetTop-50 && window.scrollY <= sections[2].offsetTop-50){
+      nav[1].children[0].classList.add("active-nav")
+    }
+    else if(window.scrollY >= sections[2].offsetTop-50 && window.scrollY <= sections[3].offsetTop-50){
+      nav[2].children[0].classList.add("active-nav")
+    }
+    else if(window.scrollY >= sections[3].offsetTop-50){
+      nav[3].children[0].classList.add("active-nav")
+    }
+}
+
