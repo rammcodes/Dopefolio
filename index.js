7c4b64ace2fb2f5b545f7d4ff713efb30a8e47eb
sebastianlarisch <sebastian.larisch@teufel.de>
2024-03-21 23:08:30 +0100

    change text, add favicon, small css adjustments

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
 const headerLogoConatiner = document.querySelector('.header__logo-container')
 
 headerLogoConatiner.addEventListener('click', () => {
-  location.href = 'index.html'
+  location.href = '/'
 })
